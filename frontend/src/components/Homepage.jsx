ba241144b16675f4a1c559a8617ec2ed2a6cd9a2
Doc-Franz <federico.franzi94@gmail.com>
2025-03-06 20:06:18 +0100

    commit finale 06_03

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,10 +1,12 @@
-import Footer from "./Footer";
+import Hero from "./Hero";
 import MyNavbar from "./MyNavbar";
 
 const HomePage = () => {
   return (
     <>
       <MyNavbar />
+      <Hero />
+      <Hero />
     </>
   );
 };
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -48,6 +48,8 @@ const Login = () => {
   useEffect(() => {
     if (isRegistered) {
       navigate("/");
+
+      dispatch(resetLoginState());
     }
   }, [isRegistered]);
 
