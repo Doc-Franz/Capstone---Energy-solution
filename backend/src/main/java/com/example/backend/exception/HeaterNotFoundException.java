7489aea7362a4f4128726e081b77637595d6b38d
Doc-Franz <federico.franzi94@gmail.com>
2025-03-10 19:29:08 +0100

    10_03 serale

---
+++ backend/src/main/java/com/example/backend/exception/HeaterNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.backend.exception;
+
+public class HeaterNotFoundException extends RuntimeException {
+  public HeaterNotFoundException(String message) {
+    super(message);
+  }
+}
