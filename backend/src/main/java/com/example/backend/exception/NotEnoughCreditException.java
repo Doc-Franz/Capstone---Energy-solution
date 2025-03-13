baa3a03e8c56cb5a6fecbfa9f59a5923833fc9df
Doc-Franz <federico.franzi94@gmail.com>
2025-03-13 22:27:05 +0100

    13_03 sera

---
+++ backend/src/main/java/com/example/backend/exception/NotEnoughCreditException.java
@@ -0,0 +1,7 @@
+package com.example.backend.exception;
+
+public class NotEnoughCreditException extends RuntimeException {
+  public NotEnoughCreditException(String message) {
+    super(message);
+  }
+}
