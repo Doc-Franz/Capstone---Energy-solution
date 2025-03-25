ac546869d760113cd0ed95664717234aa66702ef
Doc-Franz <federico.franzi94@gmail.com>
2025-03-25 01:52:02 +0100

    24_03ddd

--- backend/src/main/java/com/example/backend/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/example/backend/configuration/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "https://energysolution.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
